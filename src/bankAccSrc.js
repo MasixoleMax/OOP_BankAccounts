aa0177785eea551767c330fa7f88378e65cd8716
Masixole <masixole.menqe@umuzi.org>
2019-09-30 14:59:25 +0200

    second commit

---
+++ src/bankAccSrc.js
@@ -0,0 +1,34 @@
+class BankAccount {
+    constructor (amount, monthlyFee, interestRate){
+        this.balance = amount;
+        this.monthlyFee = monthlyFee;
+        this.interestRate = interestRate;
+       
+    };
+
+     finishMonth(){
+            var interestEarned = ((this.interestRate / 100) / 12) * this.balance;
+            this.balance = (this.balance + interestEarned - this.monthlyFee); 
+            console.log(this.balance);
+    };
+
+    deposit(amount){
+        this.balance = this.balance + amount;
+        console.log(this.balance);
+    };
+
+    withdrawal(amount){
+    this.balance = this.balance - amount;
+    console.log(this.balance);
+    };
+};
+``
+let finalBalance = new BankAccount (1000, 50, 12);
+
+
+finalBalance.finishMonth();
+finalBalance.withdrawal(100);
+finalBalance.deposit(100);
+
+
+
--- src/bankSrc.js
+++ src/bankSrc.js
@@ -1,27 +1,15 @@
-class BankAccount {
-    constructor (amount, monthlyFee, interestRate, deposit, withdrawal){
-        this.balance = amount;
-        this.monthlyFee = monthlyFee;
-        this.interestRate = interestRate;
-        this.withdrawal = withdrawal;
-        this.deposit = deposit;
-    };
+class Bank {
+    constructor(bankAccountNumber){
+        this.bankAccountNumber = bankAccountNumber;
+        
+    }
 
-     finishMonth(){
-            var interestEarned = ((this.interestRate / 100) / 12) * this.balance;
-            console.log( (this.balance + interestEarned) - this.monthlyFee); 
-    };
+    withdraw(bankAccountNumber, amount){
+     let bankAccountNumber = this.bankAccountNumber;
 
-deposit(amount){
-        this.balance = (this.balance + amount);
-        console.log(this.balance); 
-  };
-};
-
-let finalBalance1 = new BankAccount (1000, 50, 12, 40);
-let finalBalance2 = new BankAccount (500, 35, 12);
-
-    finalBalance1.finishMonth();
-    finalBalance2.deposit(100);
+     
+     
 
 
+    }
+}
