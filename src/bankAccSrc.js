c466a19b3cd6f89eee6ef5f4996711a6864f99e1
Masixole <masixole.menqe@umuzi.org>
2019-10-07 12:11:45 +0200

    Working code

--- src/bankAccSrc.js
+++ src/bankAccSrc.js
@@ -3,12 +3,13 @@ class BankAccount {
         this.balance = balance;
         this.monthlyFee = monthlyFee;
         this.interestRate = interestRate;
-       
+     
     };
 
      finishMonth(){
             var interestEarned = ((this.interestRate / 100) / 12) * this.balance;
             this.balance = (this.balance + interestEarned - this.monthlyFee); 
+            this.balance === this.balance;
             console.log(this.balance);
     };
 
@@ -18,17 +19,31 @@ class BankAccount {
     };
 
     withdrawal(amount){
-    this.balance = this.balance - amount;
-    console.log(this.balance);
+        this.balance = this.balance - amount;
+        console.log(this.balance);
+
     };
 };
-``
+
+// module.exports = BankAccount;
+
 let finalBalance = new BankAccount (1000, 50, 12);
 
-
+//Perform finishMonth()
+console.log('The final balance of the account after one month is:')
 finalBalance.finishMonth();
+console.log("");
+
+//Perform withdral()
+console.log('The final balance of the account after withdrawing the amount is:')
 finalBalance.withdrawal(100);
-finalBalance.deposit(100);
+console.log("");
+
+//Perform deposit()
+console.log('The final balance of the account after depositing the amount is:')
+finalBalance.deposit(200);
+console.log("");
+
 
 
 
--- src/bankSrc.js
+++ src/bankSrc.js
@@ -1,22 +1,81 @@
-class BankAccount {
-    constructor (balance, monthlyFee, interestRate){
+ let BankAccount = require ('./bankAccSrc');
+
+ class Bank {
+    constructor (AccNum, balance, newBalance){
+        this.AccNum = AccNum;
         this.balance = balance;
-        this.monthlyFee = monthlyFee;
-        this.interestRate = interestRate;
-       
+        while(arguments == 2){
+            this.newBalance = newBalance;
+        }
+     
     }
-};
 
-class Bank extends BankAccount {
-    constructor (balance, monthlyFee, interestRate, BankAccount ){
-        this.BankAccount = new BankAccount(balance, monthlyFee, interestRate);
-        this.BankAccount = AccNum;
-    };
-
+    
     withdraw(AccNum, amount){
-        AccNum.this.BankAccount.withdraw(amount);
-    }
-}
 
-let y = new Bank();
-console.log(y.withdraw(123456788, 100));
+        if(this.AccNum = AccNum){
+
+            let newBalance = this.balance - amount;
+            console.log(newBalance);
+
+        }   
+            else {
+
+                     throw new Error('But you have entered an invalid account number');
+        } 
+    }
+
+    deposit(AccNum, amount){
+       
+        if(this.AccNum = AccNum){
+          
+            let newBalance = this.balance + amount;
+            console.log(newBalance);
+
+        } 
+
+        else {
+            
+                throw new Error('You have entered an invalid account number');
+            
+        }
+     
+    }
+
+
+    transfer(fromBankAcc, toBankAcc, amount){
+           
+            this.deposit(toBankAcc, amount);  
+            this.withdraw(fromBankAcc, amount);  
+        } 
+
+    }
+
+
+let y = new Bank(54321, 2000);
+let x = new Bank(12345, 1000);
+
+//Perform withdrawal and deposit for x //
+console.log("The new balances of x after deposit and withdrawal respectively are:");
+
+x.deposit(x.AccNum, 200);
+x.withdraw(x.AccNum, 100);
+console.log("")
+
+
+//Perform withdrawal and deposit for y //
+console.log("The new balances of y after deposit and withdrawal respectively are:");
+
+y.deposit(y.AccNum, 200);
+y.withdraw(y.AccNum, 100);
+console.log("")
+
+//Perform a tranfer from x to y //
+console.log("The new balance of x after transfer is:");
+
+x.transfer(x.AccNum, y.AccNum, 100);
+console.log("")
+
+
+// console.log(y.balance);
+// console.log(x.balance);
